f53b7771c2c7453576dcdc59f17495b55f0d3b19
gcocce <gastoncocce@hotmail.com>
2017-04-03 20:20:25 -0300

    actualizacion de la documentacion

--- src/ledDriver.h
+++ src/ledDriver.h
@@ -1,26 +1,14 @@
 /****************************************************************************
- * Copyright (C) 2012 by Matteo Franchin                                    *
+ * Copyright (C) 2012 by Gaston Cocce                                       *
  *                                                                          *
- * This file is part of Box.                                                *
+ * Este archivo es parte del trabajo práctico para Ing de Software          *
  *                                                                          *
- *   Box is free software: you can redistribute it and/or modify it         *
- *   under the terms of the GNU Lesser General Public License as published  *
- *   by the Free Software Foundation, either version 3 of the License, or   *
- *   (at your option) any later version.                                    *
- *                                                                          *
- *   Box is distributed in the hope that it will be useful,                 *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
- *   GNU Lesser General Public License for more details.                    *
- *                                                                          *
- *   You should have received a copy of the GNU Lesser General Public       *
- *   License along with Box.  If not, see <http://www.gnu.org/licenses/>.   *
  ****************************************************************************/
 
 /**
  * @file ledDriver.h
  * @author Gastón Coccé
- * @date 3 March 2017
+ * @date 3 Apr 2017
  * @brief File containing example of doxygen usage for quick reference.
  *
  * Here typically goes a more extensive explanation of what the header
@@ -36,23 +24,23 @@
 /**
  * @brief A simple function to create the Driver.
  *
+ * @param address Es la dirección de memoria que mapea el driver
  *
- * @return @c Void is always returned.
+ * @return void is always returned.
  */
 void ledDriver_create(uint16_t * address);
 
 /**
  * @brief A simple function to turn on a specific led.
  *
- *
- * @return @c Void is always returned.
+ * @return Void is always returned.
  */
 void ledDriver_TurnLedOn(uint16_t * address /*! Address of the driver */,
 		uint16_t led /*! The led to turn on */);
 
 /*! \details This function turns off a specific led.
   *\param address is the driver's address.
- *\param led is the a 16 bits mask indicating the led to turn offc*
+ *\param led is the a 16 bits mask indicating the led to turn off
  * \return Void.
  */
 void ledDriver_TurnLedOff(uint16_t *, uint16_t led);
@@ -60,17 +48,28 @@ void ledDriver_TurnLedOff(uint16_t *, uint16_t led);
 /*! \brief Brief description.
  *         Brief description continued.
  *
+ *  \sa ledDriver_TurnAllOff()
+ *
+ *  @param address es la direccion de memoria que mapea el driver
+ *
  *  Detailed description starts here.
  */
-void ledDriver_TurnAllOn(uint16_t *);
+void ledDriver_TurnAllOn(uint16_t *address);
 
 //! @brief A simple function to turn All Leds Off.
 /*!
-  \sa testMe()
+  \sa ledDriver_TurnAllOn()
   \param address is the driver's address.
 */
 void ledDriver_TurnAllOff(uint16_t * address);
 
+/**
+ * @brief A simple function to create the Driver.
+ *
+ * @param address Es la dirección de memoria que mapea el driver
+ *
+ * @return void is always returned.
+ */
 bool ledDriver_LedStatus(uint16_t *, uint16_t led);
 
 void ledDriver_TurnMultipleLedOn(uint16_t *, uint16_t led);
